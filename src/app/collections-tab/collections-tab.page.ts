f3982c34ab8cbb091d3796d1b649ba5525aa1c38
blugate-git <kriti.vabby15@gmail.com>
2020-05-29 14:40:28 +0530

    Push as of 29 May

--- src/app/collections-tab/collections-tab.page.ts
+++ src/app/collections-tab/collections-tab.page.ts
@@ -8,6 +8,53 @@ import { Component } from '@angular/core';
 export class CollectionsTabPage {
   selector: any = [];
 
+  sliderConfig = {
+    slidesPerView: 3,
+    spaceBetween: 2,
+    centeredSlides: false
+  };
+  public images = [
+    {
+      product: "assets/images/dd.jpg",
+      tag: "Mac palette",
+      posts: "$ 89"
+    },
+    {
+      product: "assets/images/aa.jpg",
+      tag: "#JasmineMaria",
+      posts: "$ 20"
+    },
+    {
+      product: "assets/images/dd.jpg",
+      tag: "Skin products",
+      posts: "$ 20"
+    },
+    {
+      product: "assets/images/bb.jpg",
+      tag: "Bobbibrown",
+      posts: "$ 46"
+    },
+    {
+      product: "assets/images/aa.jpg",
+      tag: "Collection",
+      posts: "$ 54"
+    },
+    {
+      product: "assets/images/cc.jpg",
+      tag: "JasmineMaria",
+      posts: "$ 29"
+    },
+    {
+      product: "assets/images/dd.jpg",
+      tag: "makeup",
+      posts: "$ 35"
+    },
+    {
+      product: "assets/images/bb.jpg",
+      tag: "bobbibrown",
+      posts: "$ 15"
+    },
+  ]
   constructor() {
     this.selector = "Current";
   }
