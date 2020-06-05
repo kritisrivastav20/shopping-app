84eb916e537955e236eca244def7e3f68f38fcc2
blugate-git <kriti.vabby15@gmail.com>
2020-06-05 08:38:16 +0530

    User Profile

--- src/app/collections-tab/collections-tab.page.ts
+++ src/app/collections-tab/collections-tab.page.ts
@@ -21,7 +21,7 @@ export class CollectionsTabPage {
     },
     {
       product: "assets/images/aa.jpg",
-      tag: "#JasmineMaria",
+      tag: "JasmineMaria",
       posts: "$ 20"
     },
     {
@@ -55,6 +55,64 @@ export class CollectionsTabPage {
       posts: "$ 15"
     },
   ]
+
+  public posts = [
+    {
+      name: 'Valeria Lipovetsky',
+      profileImg: 'assets/images/avatar.svg',
+      postImg: 'assets/images/post.svg',
+      isFollow: true,
+      isLiked: true,
+      description: `Sneak a peek into the closet`,
+      tags: ['instyle', 'mensfashion', 'casual', 'casualstyle'],
+      time: '35 min ago',
+      totalLikes: '2.7k',
+      tag: "Mac palette",
+      posts: "$ 89"
+    },
+    {
+      name: 'Liliana Ava',
+      profileImg: 'assets/images/avatar2.png',
+      postImg: 'assets/images/post-img/fash2.jpeg',
+      isFollow: false,
+      isLiked: false,
+      description: `Sneak a peek into the closet of literally any guy you know,
+        and you’ll see some combination of dark-wash...`,
+      tags: ['instyle', 'mensfashion', 'casual', 'casualstyle'],
+      time: '35 min ago',
+      totalLikes: '2.7k',
+      tag: "Mac palette",
+      posts: "$ 89"
+    },
+    {
+      name: 'Jessica Barbara',
+      profileImg: 'assets/images/avatar3.png',
+      postImg: 'assets/images/post-img/fash3.jpg',
+      isFollow: true,
+      isLiked: false,
+      description: `Sneak a peek into the closet of literally any guy you know,
+        and you’ll see some combination of dark-wash...`,
+      tags: ['instyle', 'mensfashion', 'casual', 'casualstyle'],
+      time: '35 min ago',
+      totalLikes: '2.7k',
+      tag: "Mac palette",
+      posts: "$ 89"
+    },
+    {
+      name: 'Jennifer Elizabeth',
+      profileImg: 'assets/images/avatar4.png',
+      postImg: 'assets/images/post-img/fash4.jpg',
+      isFollow: false,
+      isLiked: true,
+      description: `Sneak a peek into the closet of literally any guy you know,
+        and you’ll see some combination of dark-wash...`,
+      tags: ['instyle', 'mensfashion', 'casual', 'casualstyle'],
+      time: '35 min ago',
+      totalLikes: '2.7k',
+      tag: "Mac palette",
+      posts: "$ 89"
+    }
+  ];
   constructor() {
     this.selector = "Current";
   }
--- src/app/menu-tab/menu-tab.page.ts
+++ src/app/menu-tab/menu-tab.page.ts
@@ -7,6 +7,52 @@ import { Component } from '@angular/core';
 })
 export class MenuTabPage {
 
-  constructor() {}
+  selector: any;
+  public images = [
+    {
+      product: "assets/images/dd.jpg",
+      tag: "#bobbibrown",
+      posts: "15.6K posts"
+    },
+    {
+      product: "assets/images/aa.jpg",
+      tag: "#JasmineMaria",
+      posts: "12K posts"
+    },
+    {
+      product: "assets/images/dd.jpg",
+      tag: "#makeupCollection",
+      posts: "2K posts"
+    },
+    {
+      product: "assets/images/bb.jpg",
+      tag: "#bobbibrown",
+      posts: "15.6K posts"
+    },
+    {
+      product: "assets/images/aa.jpg",
+      tag: "#bobbibrown",
+      posts: "15.6K posts"
+    },
+    {
+      product: "assets/images/cc.jpg",
+      tag: "#JasmineMaria",
+      posts: "12K posts"
+    },
+    {
+      product: "assets/images/dd.jpg",
+      tag: "#makeupCollection",
+      posts: "2K posts"
+    },
+    {
+      product: "assets/images/bb.jpg",
+      tag: "#bobbibrown",
+      posts: "15.6K posts"
+    },
+  ]
+
+  constructor() {
+    this.selector = "Current"
+  }
 
 }
